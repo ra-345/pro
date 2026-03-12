e06af2f3bc2c2b8998ce089cbbd89f7f651851e6
ra-345 <techwithsai3@gmail.com>
2026-03-12 22:10:59 +0530

    Create 3b.java

---
+++ 3b.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class SubstitutionCipher {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = "abcdefghijklmnopqrstuvwxyz";
+        String b = "zyxwvutsrqponmlkjihgfedcba";
+
+        System.out.print("Enter any string: ");
+        String str = br.readLine().toLowerCase();
+
+        String encrypt = "";
+        char c;
+
+        // Encryption
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            int j = a.indexOf(c);
+
+            if (j != -1) {
+                encrypt = encrypt + b.charAt(j);
+            } else {
+                encrypt = encrypt + c; // keep spaces/symbols
+            }
+        }
+
+        System.out.println("Encrypted data: " + encrypt);
+
+        // Decryption
+        String decrypt = "";
+
+        for (int i = 0; i < encrypt.length(); i++) {
+            c = encrypt.charAt(i);
+            int j = b.indexOf(c);
+
+            if (j != -1) {
+                decrypt = decrypt + a.charAt(j);
+            } else {
+                decrypt = decrypt + c;
+            }
+        }
+
+        System.out.println("Decrypted data: " + decrypt);
+    }
+}
